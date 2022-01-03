304d5d4a9f22b2bf01d5a2db1ac4aae1c3a1b1fb
RobotWizard <harikrishnan527@gmail.com>
2022-01-03 17:14:38 +0530

    exponential calculation in linear time ~(^-^)~

--- 6/1.py
+++ 6/1.py
@@ -1,18 +1,25 @@
-fishies = []
+from collections import defaultdict
+
+fishies = [0]*9
 step = []
 
 with open('./6/input') as f:
-    fishies = [(int(x), 0) for x in f.readline().split(',')]
+    for x in f.readline().split(','):
+        fishies[int(x)] += 1
 
+print(fishies)
+count = 0
 for _ in range(256):
-    step = []
-    for f, cycle in fishies:
-        if f == 0:
-            step.append((6, cycle+1))
-            step.append((8, 0))
+    step = [0]*9
+    for idx, f in enumerate(fishies):
+        if idx == 0:
+            step[8] = fishies[0]
+            step[6] = fishies[0]
         else:
-            step.append((f-1, cycle+1))
+            step[idx-1] += fishies[idx]
 
     fishies = step
+    # print(sum(v for k, v in fishies.items()))
+    # print(fishies)
 
-print(len(fishies))
+print(sum(fishies))
